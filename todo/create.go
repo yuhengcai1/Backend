dcc0b72ab30ef73246a1e6c0bedb85cbcc322cd3
Yuheng Cai <97816565+yuhengcai1@users.noreply.github.com>
2023-09-12 19:45:56 -0400

    Update create.go

--- todo/create.go
+++ todo/create.go
@@ -2,24 +2,19 @@ package todo
 
 import "github.com/gin-gonic/gin"
 
-// this variable is used to generate unique ids for todos
-var idCounter int = 1
+var idocount int = 1
 
-// This is a function to create a new todo
 func createTodo(c *gin.Context) {
 	var todo Todo
-	// bind the json body to the todo struct
 	c.BindJSON(&todo)
-	// check if the title is empty
 	if todo.Title == "" {
 		c.JSON(400, gin.H{"error": "title is required"})
 		return
 	} else {
 		// set the id
-		todo.ID = idCounter
+		todo.ID = idocount
 		todos = append(todos, todo)
-		// increment the idCounter
-		idCounter++
+		idocount++
 		c.JSON(200, todo)
 	}
 }
