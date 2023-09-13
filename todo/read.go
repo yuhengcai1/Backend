cbcb9392ee3971c5abee7e946fbb95b5e97a39b1
Yuheng Cai <97816565+yuhengcai1@users.noreply.github.com>
2023-09-12 21:47:02 -0400

    Update read.go

--- todo/read.go
+++ todo/read.go
@@ -9,15 +9,10 @@ func readTodo(c *gin.Context) {
 	id := c.Param("id")
 	todoId, _:= strconv.Atoi(id)
 	for _, todo := range todos {
-		// if the id matches the id in the todo
 		if todo.ID == todoId {
 			c.JSON(200, todo)
 			return
 		}
 	}
 	c.JSON(404, gin.H{"error": "None"})
-}
-
-func listTodos(c *gin.Context) {
-	c.JSON(200, todos)
 }
