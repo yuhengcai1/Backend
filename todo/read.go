48bdde3a56c3c8bac319a1d4bd03ee43f740ba2c
Yuheng Cai <97816565+yuhengcai1@users.noreply.github.com>
2023-09-12 21:44:13 -0400

    Update read.go

--- todo/read.go
+++ todo/read.go
@@ -2,21 +2,12 @@ package todo
 
 import (
 	"strconv"
-
 	"github.com/gin-gonic/gin"
 )
 
-// this function is responsible for fetching a todo by id
 func readTodo(c *gin.Context) {
-	// get the id from the url
 	id := c.Param("id")
-	// convert the id to an integer
-	todoId, err := strconv.Atoi(id)
-	if err != nil {
-		c.JSON(400, gin.H{"error": "invalid id"})
-		return
-	}
-	// loop through the todos
+	todoId, _:= strconv.Atoi(id)
 	for _, todo := range todos {
 		// if the id matches the id in the todo
 		if todo.ID == todoId {
@@ -24,10 +15,9 @@ func readTodo(c *gin.Context) {
 			return
 		}
 	}
-	c.JSON(404, gin.H{"error": "todo not found"})
+	c.JSON(404, gin.H{"error": "None"})
 }
 
-// this function is responsible for fetching all todos
 func listTodos(c *gin.Context) {
 	c.JSON(200, todos)
 }
