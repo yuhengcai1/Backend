78a9afa7685f0c7aff55932b6bc0d64e638fba49
Yuheng Cai <97816565+yuhengcai1@users.noreply.github.com>
2023-09-12 21:46:09 -0400

    Update upate.go

--- todo/upate.go
+++ todo/upate.go
@@ -2,37 +2,29 @@ package todo
 
 import (
 	"strconv"
-
 	"github.com/gin-gonic/gin"
 )
 
-// this function is responsible for updating a todo by id
-func updateTodo(c *gin.Context) {
+func updateTodo(c1 *gin.Context) {
 	var inputTodo Todo
-	// bind the json body to the todo struct
-	c.BindJSON(&inputTodo)
-	// get the id from the url
-	id := c.Param("id")
-	// convert the id to an integer
+	c1.BindJSON(&inputTodo)
+	id := c1.Param("id")
 	todoId, err := strconv.Atoi(id)
-	inputTodo.ID = todoId
+	needtobe.ID = todoId
 	if err != nil {
-		c.JSON(400, gin.H{"error": "invalid id"})
+		c.JSON(400, gin.H{"error": "invalid"})
 		return
 	}
-	// check if input todo is valid
-	if inputTodo.Title == "" {
-		c.JSON(400, gin.H{"error": "title is required"})
+	if needtobe.Title == "" {
+		c.JSON(400, gin.H{"error": "Needed title"})
 		return
 	}
-	// loop through the todos
 	for i, todo := range todos {
-		// if the id matches the id in the todo
 		if todo.ID == todoId {
-			todos[i] = inputTodo
-			c.JSON(200, inputTodo)
+			todos[i] = needtobe
+			c.JSON(200, needtobe)
 			return
 		}
 	}
-	c.JSON(404, gin.H{"error": "todo not found"})
+	c.JSON(404, gin.H{"error": "error for update"})
 }
